b0c80d4376ddab0a31c197737a76b9df8d3fde09
John DYER <jdyer@e1r8p9.42.fr>
2021-10-07 18:46:49 +0200

    debut tests unitaires

--- middlewares/main.cpp
+++ middlewares/main.cpp
@@ -1,21 +1,48 @@
 #include "Middlewares.hpp"
 #include <list>
 
-int	main(void) {
-	char *str = "GET           /test.html HTTP/1.1\r\nDate: ok\r\nContent-Length: non\r\n\r\nretest: ah\r\n";
+void	test_check_syntax(char *str) {
+
 	Request test;
 	std::list<IMiddlewares *> middlewares;
 	std::list<IMiddlewares *>::iterator it;
+	std::map<std::string, std::string>::iterator it2;
 	CheckSyntax	csx;
+
+	std::cout << "------------------------" << std::endl;
+	std::cout << "Requete :" << std::endl;
+	std::cout << str << std::endl;
+	std::cout << "------------------------" << std::endl;
+
 	while (test.get_head() != 4) {
 		str = test.parse(str);
 	}
+
+	std::cout << "------------------------" << std::endl;
+	std::cout << "method: " << test.get_method() << std::endl;
+	std::cout << "path: " << test.get_path() << std::endl;
+	std::cout << "protocol: " << test.get_protocol() << std::endl;
+	std::cout << "///HEADERS" << std::endl;
+	for (it2 = (*test.get_header()).begin(); it2 != (*test.get_header()).end(); ++it2)
+		std::cout << it2->first << ": " << it2->second << std::endl;
+	std::cout << "///" << std::endl;
+	std::cout << "body: " << test.get_body() << std::endl;
+	std::cout << "------------------------" << std::endl;
 	middlewares.push_back(&csx);
 	try {
-		for (it = middlewares.begin(); it != middlewares.end(); ++it){
+		for (it = middlewares.begin(); it != middlewares.end(); ++it) {
 			(*it)->body(test);
 		}
 	}
 	catch (int code) {test.set_code(code);}
 	std::cout << test.get_code() << std::endl;
+}
+
+int	main(void) {
+	test_check_syntax("GET           /test.html HTTP/1.1\r\nDate: ok\r\nContent-Length: non\r\n\r\nretest: ah\r\n\r\n");
+	test_check_syntax("GET           test.html HTTP/1.1\r\nDate: ok\r\nContent-Length: non\r\n\r\nretest: ah\r\n\r\n");
+	test_check_syntax("GET           /test.html HTTP/1.1\r\nDate: ok\r\nContent-Length: non\r\n\r\nretest: ah\r\n");
+
+	test_check_syntax("GET  n\r\nretest: ah\r\n\r\n");
+
 }
