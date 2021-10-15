b2542180fc7952d04291c6c99ba9a8f26b8726d5
Simon MOULIN <simoulin@e1r4p8.42.fr>
2021-10-15 16:35:11 +0200

    Changes 10/15/21

--- srcs/server/serverConfig.cpp
+++ srcs/server/serverConfig.cpp
@@ -1,7 +1,7 @@
 #include "../../includes/serverConfig.hpp"
 
 
-ServerConfig::ServerConfig() : _host("0.0.0.0"), _name(), _error(), _server_root(), _port(80){}
+ServerConfig::ServerConfig() : _host(0), _name(), _error(), _server_root(), _port(80){}
 
 ServerConfig::ServerConfig(const ServerConfig &other) : _host(other._host), _name(other._name), _error(other._error), _server_root(other._server_root), _port(other._port)  {}
 ServerConfig::~ServerConfig(){}
@@ -14,13 +14,13 @@ ServerConfig &ServerConfig::operator=(const ServerConfig &other)
 }
 
 void            ServerConfig::set_port(const int port){ this->_port = port; }
-void            ServerConfig::set_host(std::string const &host){ this->_host = host; }
+void            ServerConfig::set_host(const uint32_t host){ this->_host = host; }
 void            ServerConfig::set_name(std::string const &name){ this->_name = name; }
 void            ServerConfig::set_error(std::string const &error){ this->_error = error; }
 void            ServerConfig::set_root(std::string const &server_root){ this->_server_root = server_root; }
 
 int                  ServerConfig::get_port(void){ return (this->_port); }
-std::string const   &ServerConfig::get_host(void){ return (this->_host); }
+uint32_t             ServerConfig::get_host(void){ return (this->_host); }
 std::string const   &ServerConfig::get_name(void){ return (this->_name); }
 std::string const   &ServerConfig::get_error(void){ return (this->_error); }
 
