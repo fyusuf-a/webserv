28d5fa1be6040296e99d7a809a3db62f1fbec78a
fyusuf-a <fyusuf-a@student.42.fr>
2021-10-30 13:07:50 +0000

    Put launching of servers in the constructor of webserv

--- srcs/webserv/webserv.cpp
+++ srcs/webserv/webserv.cpp
@@ -9,6 +9,12 @@ WebServ::WebServ(const WebServ &other){(void)other;};
 
 WebServ::WebServ(const std::string&path){
 	init(path);
+	for (std::vector<ServerBlock>::iterator it = _servers.begin(); it != _servers.end(); it++)
+	{
+	 	ServerConfig const& conf = it->get_server_conf();
+        INetAddress interface(conf.get_host(), conf.get_port());
+	 	new PassiveHTTP<ActiveServer>(interface, *it, true);
+	}
 };
 
 WebServ::~WebServ(){};
@@ -20,15 +26,7 @@ WebServ &WebServ::operator=(const WebServ &other){(void)other;return *this;}
 
 void WebServ::init(const std::string& path)
 {
-
     _conf.parsing(path, this->_servers);
-
-	for (std::vector<ServerBlock>::iterator it = _servers.begin(); it != _servers.end(); it++)
-	{
-	 	ServerConfig const& conf = it->get_server_conf();
-        INetAddress interface(conf.get_host(), conf.get_port());
-	 	new PassiveHTTP<ActiveServer>(interface, *it, true);
-	}
 }
 
 ServerBlocks const& WebServ::get_servers() const {
