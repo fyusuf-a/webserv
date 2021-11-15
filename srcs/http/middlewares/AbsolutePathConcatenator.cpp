beb7a367d3857969d3b950f36302f69168a0e5bc
John DYER <jdyer@e1r7p2.42.fr>
2021-11-15 17:39:25 +0100

    APC a tester

--- srcs/http/middlewares/AbsolutePathConcatenator.cpp
+++ srcs/http/middlewares/AbsolutePathConcatenator.cpp
@@ -5,4 +5,18 @@ void		AbsolutePathConcatenator::body(ActiveHTTP& actHTTP, Request& request, Resp
 	(void)response;
 	(void)actHTTP;
 
+    if (response.get_code() >= 400)
+        next();
+
+	std::string absolute_path;
+
+	if (request.get_location().get_root() != "")
+		absolute_path = request.get_location().get_root() + request.get_path();
+	else if (request.get_server().get_server_conf().getRoot() == "")
+		absolute_path = "";
+	else
+		absolute_path = request.get_server().get_server_conf().getRoot() + request.get_path();
+
+	request.set_path(absolute_path);
+	next();
 }
