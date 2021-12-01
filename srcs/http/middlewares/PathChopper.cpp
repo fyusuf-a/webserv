f50097c5222b53a7d09fe335b697db88826b5538
John DYER <jdyer@e1r13p12.42.fr>
2021-12-01 14:56:34 +0100

    go

--- srcs/http/middlewares/PathChopper.cpp
+++ srcs/http/middlewares/PathChopper.cpp
@@ -43,8 +43,9 @@ void		PathChopper::body(ActiveHTTP&, Request& request, Response& response, Middl
             break;
         head = loc + 1;
     }
-
     request.set_extra_path(path.substr(loc + cgi_ext.length()));
     request.set_path(path.substr(0, loc + cgi_ext.length()));
+    std::cout << request.get_path() << "||" << request.get_extra_path() << "||" << request.get_query() << std::endl;
+
     next();
 }
