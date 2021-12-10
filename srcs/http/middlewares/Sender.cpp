728c4536ab6078be32e16cf5ff6babf32e223919
fyusuf-a <fyusuf-a@student.42.fr>
2021-12-10 12:14:11 +0100

    Added next to Sender middleware

--- srcs/http/middlewares/Sender.cpp
+++ srcs/http/middlewares/Sender.cpp
@@ -3,7 +3,7 @@
 Sender::Sender() {
 }
 
-void Sender::body(ActiveHTTP& serv, Request&, Response& resp, MiddlewareChain&) {
+void Sender::body(ActiveHTTP& serv, Request&, Response& resp, MiddlewareChain& next) {
 	if (!serv.get_ongoing_task()) {
 		std::map<std::string, std::string> const& my_map = resp.get_headers();
 		std::map<std::string, std::string>::const_iterator it = my_map.find("Content-Length");
@@ -17,4 +17,5 @@ void Sender::body(ActiveHTTP& serv, Request&, Response& resp, MiddlewareChain&) 
 	}
 	//std::cout << resp.get_body()<<std::endl;;
 	resp.ready();
+	next();
 }
